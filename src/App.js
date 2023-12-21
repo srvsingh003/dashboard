98a190c8ee4a5933668c24633d9a25248fc1f814
saurabh singh <srvsingh003@gmail.com>
2023-12-21 23:16:56 +0530

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 function App() {
   return (
-    <div className="h-[400px] bg-red-400" style={{ border: "1px solid red" }}>
+    <div
+      className="h-[400px] bg-red-400 text-4xl"
+      style={{ border: "1px solid red" }}
+    >
       Happy Coidng !
     </div>
   );
